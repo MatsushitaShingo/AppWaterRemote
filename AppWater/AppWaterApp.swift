f0009232a0f45b1338bce27be010a1aaa0d5f9be
松下慎吾 <s-matsushita@ilab.pu-kumamoto.ac.jp>
2022-03-15 14:32:57 +0900

    Initial Commit

---
+++ AppWater/AppWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWaterApp.swift
+//  AppWater
+//
+//  Created by 松下慎吾 on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct AppWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
