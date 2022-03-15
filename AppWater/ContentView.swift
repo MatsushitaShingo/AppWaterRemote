8a4a44c48199ff989c84140c081873107d4c69ee
MatsushitaShingo <s-matsushita@ilab,pu-kumamoto.co.jp>
2022-03-15 15:53:04 +0900

    ContentView修正

--- AppWater/ContentView.swift
+++ AppWater/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
-            .padding()
+        NavigationView{
+                          HStack{
+                              NavigationLink(destination: MovieView()){
+                                  ZStack{
+                                      Image(systemName: "drop.fill")
+                                          .resizable()
+                                          .scaledToFit()
+                                          .frame(width: 200, height: 200)
+                                      Text("動画再生機能")
+                                          .padding(.vertical)
+                                          .foregroundColor(.white)
+                                  }
+                              }
+                              NavigationLink(destination: StudyView()){
+                                  ZStack{
+                                      Image(systemName: "drop.fill")
+                                          .resizable()
+                                          .scaledToFit()
+                                          .frame(width: 200, height: 200)
+                                      Text("学習機能")
+                                          .padding(.vertical)
+                                          .foregroundColor(.white)
+                                  }
+                              }
+                              NavigationLink(destination: Visualization()){
+                                  ZStack{
+                                      Image(systemName: "drop.fill")
+                                          .resizable()
+                                          .scaledToFit()
+                                          .frame(width: 200, height: 200)
+                                      Text("可視化機能")
+                                          .padding(.vertical)
+                                          .foregroundColor(.white)
+                                  }
+                              }
+                          }
+                      }.navigationViewStyle(.stack)
     }
 }
 
