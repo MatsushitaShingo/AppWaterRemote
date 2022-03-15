5f186a37a1764ac84cdb6847f723459202445983
松下慎吾 <s-matsushita@ilab.pu-kumamoto.ac.jp>
2022-03-15 14:56:28 +0900

    ContentViewにナビゲーションビュー追加

--- AppWater/ContentView.swift
+++ AppWater/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+                    HStack{
+                        NavigationLink(destination: MovieView()){
+                            ZStack{
+                                Image(systemName: "drop.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 200, height: 200)
+                                Text("動画再生機能")
+                                    .padding(.vertical)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        NavigationLink(destination: StudyView()){
+                            ZStack{
+                                Image(systemName: "drop.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 200, height: 200)
+                                Text("学習機能")
+                                    .padding(.vertical)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        NavigationLink(destination: Visualization()){
+                            ZStack{
+                                Image(systemName: "drop.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 200, height: 200)
+                                Text("可視化機能")
+                                    .padding(.vertical)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                }.navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
---
+++ AppWater/Visualization.swift
@@ -0,0 +1,20 @@
+//
+//  Visualization.swift
+//  AppWater
+//
+//  Created by 松下慎吾 on 2022/03/15.
+//
+
+import SwiftUI
+
+struct Visualization: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Visualization_Previews: PreviewProvider {
+    static var previews: some View {
+        Visualization()
+    }
+}
