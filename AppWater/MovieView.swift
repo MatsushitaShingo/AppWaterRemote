b35376b2abb39230e630cd8a0ff38f9fc0f69cca
松下慎吾 <s-matsushita@ilab.pu-kumamoto.ac.jp>
2022-03-15 16:32:53 +0900

    WaterGrAppと同じように変更

--- AppWater/MovieView.swift
+++ AppWater/MovieView.swift
@@ -6,11 +6,139 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct MovieView: View {
+    @State var showingDetail1 = false
+    @State var showingDetail2 = false
+//    let player = URL(fileURLWithPath: Bundle.main.path(forResource:"movie",ofType: "mp4")!)
+    
+     
+     
+    
     var body: some View {
-        Text("hello,world")
+        ScrollView{
+            VStack{
+                HStack{
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                    }){
+                        Image("夜景")
+                    }.sheet(isPresented: $showingDetail1){
+                        moviestart()
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                    }){
+                        Image("飛行機窓")
+                    }.sheet(isPresented: $showingDetail2){
+                        moviestart1()
+                    }
+                }
+                HStack{
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                    }){
+                        Image("夜景")
+                    }.sheet(isPresented: $showingDetail1){
+                        moviestart()
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                    }){
+                        Image("飛行機窓")
+                    }.sheet(isPresented: $showingDetail2){
+                        moviestart1()
+                    }
+                }
+
+                HStack{
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                    }){
+                        Image("夜景")
+                    }.sheet(isPresented: $showingDetail1){
+                        moviestart()
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                    }){
+                        Image("飛行機窓")
+                    }.sheet(isPresented: $showingDetail2){
+                        moviestart1()
+                    }
+                }
+                HStack{
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                    }){
+                        Image("夜景")
+                    }.sheet(isPresented: $showingDetail1){
+                        moviestart()
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                    }){
+                        Image("飛行機窓")
+                    }.sheet(isPresented: $showingDetail2){
+                        moviestart1()
+                    }
+                }
+
+                HStack{
+                    Button(action: {
+                        self.showingDetail1.toggle()
+                    }){
+                        Image("夜景")
+                    }.sheet(isPresented: $showingDetail1){
+                        moviestart()
+                    }
+                    
+                    Button(action: {
+                        self.showingDetail2.toggle()
+                    }){
+                        Image("飛行機窓")
+                    }.sheet(isPresented: $showingDetail2){
+                        moviestart1()
+                    }
+                }
+        }
     }
+}
+struct moviestart: View{
+    
+     private let player = AVPlayer(url: Bundle.main.url(forResource:"movie",withExtension: "mp4")!)
+    
+     var body: some View{
+        VideoPlayer(player: player)
+            .onAppear(){
+                player.play()
+                
+            }
+            .onDisappear(){
+                player.pause()
+            }
+        }
+    }
+    struct moviestart1: View{
+        
+         private let player1 = AVPlayer(url: Bundle.main.url(forResource:"mov_hts-samp001",withExtension: "mp4")!)
+        
+         var body: some View{
+            VideoPlayer(player: player1)
+                .onAppear(){
+                    player1.play()
+                    
+                }
+                .onDisappear(){
+                    player1.pause()
+                }
+            }
+        }
 }
 
 struct MovieView_Previews: PreviewProvider {
--- AppWater/Visualization.swift
+++ AppWater/Visualization.swift
@@ -9,9 +9,68 @@ import SwiftUI
 
 struct Visualization: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack{
+                Text("現在の節水料")
+                               .font(.largeTitle)
+                               .bold()
+
+                Text("0L")
+                Text("○○月○○日")
+                    .padding()
+
+            }.frame(width: 250, height:160, alignment: .bottomTrailing)
+                .border(Color.red, width: 2) .offset(x:-400,y:-200)
+            Image("夜景").clipShape(Circle())
+
+            Button(action:{
+                //ボタンが押されたときのアクション
+            }){
+                ZStack{
+                    Color.blue
+                        .frame(width:110,height: 110)
+                        .overlay(Text("選ぶ").foregroundColor(.red))
+                }.offset(x:400,y:200)
+            }
+            Text("コップいっぱい")
+                .offset(y:200)
+        }
     }
 }
+struct selectView: View{
+    var body: some View{
+        VStack{
+
+            Text("記録する")
+            HStack{
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+            }
+        }
+    }
+}
+struct testView:View{
+    @State private var isShowingView: Bool = false
+
+        var body: some View {
+            if isShowingView {
+                selectView()
+            } else {
+                Button {
+                    isShowingView.toggle()
+                } label: {
+                    Text("フラグ切替")
+                }
+            }
+        }
+}
+
 
 struct Visualization_Previews: PreviewProvider {
     static var previews: some View {
